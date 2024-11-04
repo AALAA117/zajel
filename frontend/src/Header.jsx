bc9a05309e69df41cb5c85a5ff65d1d105299ff3
aalaa <alaamglal@gmail.com>
2024-11-04 21:18:47 +0200

    add go to chat icon

--- frontend/src/Header.jsx
+++ frontend/src/Header.jsx
@@ -3,7 +3,7 @@ import LOGOUT from "./assets/logout.webp";
 import AuthContext from "./context/AuthContext";
 import Friendlist from "./friends/Friendlist";
 import "./Header.css";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function Header() {
   const { logoutUser } = useContext(AuthContext);
@@ -11,18 +11,29 @@ function Header() {
 
   const handleLogout = () => logoutUser();
   const goToProfile = () => navigate("/profile");
+  const goToChat = () => navigate("/");
+  const location = useLocation();
 
   return (
-    <headers className="upperpart">
+    <header className="upperpart">
       <h1 className="title">zajel</h1>
       <div className="button-container">
-        <Friendlist />
-        <button onClick={goToProfile}>Profile</button>
+        {location.pathname === "/" && <Friendlist />}
+        {location.pathname === "/" && (
+          <button className="home-button" onClick={goToProfile}>
+            Profile Pad
+          </button>
+        )}
+        {location.pathname === "/profile" && (
+          <button onClick={goToChat} className="home-button">
+            Let's Chat
+          </button>
+        )}
         <button className="logout" onClick={handleLogout}>
           <img src={LOGOUT} alt="Logout" width="35" height="35" />
         </button>
       </div>
-    </headers>
+    </header>
   );
 }
 
