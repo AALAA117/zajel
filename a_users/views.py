3ae6e06710a7e28a2cdb4a7e94b3880a0ba1f4cc
abdallhfattah <abdallh.fattah33@gmail.com>
2024-11-05 12:26:30 +0200

    Filter users to display only those that can be added as friends.

--- a_users/views.py
+++ a_users/views.py
@@ -7,7 +7,7 @@ from .models import Profile
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
-
+from a_friends.models import FriendList , FriendRequest
 
 class ProfileViewSet(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
@@ -32,3 +32,18 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    # filtering users that are [not friends, not pending requests, not the current user]
+    def list(self, request, *args, **kwargs):
+        current_user = request.user
+        friend_list = FriendList.objects.get(user=current_user)
+        
+        friend_ids = friend_list.friends.values_list('id', flat=True)
+        sent_request_ids = FriendRequest.objects.filter(sender=current_user).values_list('receiver_id', flat=True)
+        received_request_ids = FriendRequest.objects.filter(receiver=current_user).values_list('sender_id', flat=True)
+
+        exclude_ids = list(friend_ids) + list(sent_request_ids) + list(received_request_ids) + [current_user.id]
+
+        self.queryset = self.queryset.exclude(id__in=exclude_ids)
+        
+        return super().list(request, *args, **kwargs)
